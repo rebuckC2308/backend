141cdbf88f6a9419279e48364abd0c23b1c65f8a
Chris Rebuck <rebuckc2308@gmail.com>
2022-02-08 09:51:58 -0600

    CreatingContests - updated to have function that generates a 5 digit code for all contests upon contest creation

--- contest/contest.js
+++ contest/contest.js
@@ -1,8 +1,19 @@
 const { query } = require('../queries/queries');
 
+const genRandomNumber = (length) => {
+  const numbers = '1234567890';
+  // can add characters to this string as well to make more code combinations
+  let result = '';
+  // eslint-disable-next-line no-plusplus
+  for (let i = length; i > 0; --i) {
+    result += numbers[Math.round(Math.random() * (numbers.length - 1))];
+  }
+  return Number(result);
+};
+
 async function contest(req, res, client) {
   const { body } = req;
-  const contestId = Math.floor((Math.random() * 99999) + 1);
+  const contestId = genRandomNumber(5);
   const errorMessage = 'Error creating new contest';
   try {
     await client.query(
