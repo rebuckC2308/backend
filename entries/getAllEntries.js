e7397c2d5f7d51207bd7bd8181e0e8d7dbce636b
Chris Rebuck <rebuckc2308@gmail.com>
2022-04-04 16:55:18 -0500

    in progress: getting image from S3 bucket to display in app

--- entries/getAllEntries.js
+++ entries/getAllEntries.js
@@ -1,4 +1,11 @@
+/* eslint-disable max-len */
+const AWS = require('aws-sdk');
 const { query } = require('../queries/queries');
+
+const s3 = new AWS.S3({
+  accessKeyId: AWS.config.credentials.accessKeyId,
+  secretAccessKey: AWS.config.credentials.secretAccessKey,
+});
 
 async function getAllEntries(req, res, client) {
   const { body } = req;
@@ -9,11 +16,32 @@ async function getAllEntries(req, res, client) {
 
   const entriesArray = result.rows;
 
+  const imageurl = s3.getSignedUrl('getObject', {
+    Bucket: process.env.BUCKET,
+    Key: entriesArray[0].imageurl,
+  });
+
+  console.log(imageurl);
+  //   const entriesAndImagesArray = entriesArray.map((entry) => {
+  //     console.log(entry.imageurl);
+  //     const newImageURL = s3.getSignedUrl('getObject', {
+  //       Bucket: process.env.BUCKET,
+  //       Key: entry.imageurl,
+  //     });
+
+  //     return {
+  //       // eslint-disable-next-line max-len
+  //       id: entry.id, contestid: entry.contestid, name: entry.name, imageurl: newImageURL, description: entry.description,
+  //     };
+  //   });
+  //   console.log(entriesAndImagesArray[0]);
+
+  const data = { ...entriesArray[0], imageurl };
   if (!entriesArray) {
     return res.status(422).send({ errorMessage: 'This contest does not have entries yet!' });
   }
 
-  return res.status(200).send(entriesArray);
+  return res.status(200).send([data]);
 }
 
 module.exports = { getAllEntries };
